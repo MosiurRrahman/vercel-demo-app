8fffdf631f553f09250b6f6783da7dfaa9983e95
MosiurRrahman <mosiurrahman595@gmail.com>
2023-05-23 16:08:03 +0600

    dasdhasiuodas

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require('./scripts/generate-sitemap');
-    }
-
-    return config;
-  },
 }
 
 module.exports = nextConfig
